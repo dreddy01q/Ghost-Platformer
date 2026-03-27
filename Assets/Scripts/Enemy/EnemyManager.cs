6bba6b0ddac2a513291382ee44bb225b262863c5
dreddy01 <dreddy01@qub.ac.uk>
2026-03-27 12:15:47 +0000

    Re adding of enemies

--- Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -4,6 +4,11 @@ public class EnemyManager : MonoBehaviour
 {
     public EnemyController[] enemies;
 
+    private void Awake()
+    {
+        enemies=GetComponentsInChildren<EnemyController>();
+    }
+
     // Update is called once per frame
     void Update()
     {
